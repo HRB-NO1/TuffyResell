49745585f1b68115be9a69081fe70326ea468766
KeithSUn <54606160+HRB-NO1@users.noreply.github.com>
2022-11-16 04:11:29 -0800

    Add files via upload

---
+++ TuffyResell/twittor/models/tweet.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from twittor import db
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(20))
+    body = db.Column(db.String(140))
+    price = db.Column(db.String(10))
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "id={}, body={}, create_time={}, user_id={}".format(
+            self.id, self.body, self.create_time, self.user_id
+        )
+    # 测试程序懒得写了
